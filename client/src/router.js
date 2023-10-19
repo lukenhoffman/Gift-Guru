4427759bdc027aeb50b6df95079fb0f39ae24c75
Luken Hoffman <lukenhoffman@gmail.com>
2023-10-18 20:58:30 -0400

    attempt to fix register page

--- client/src/router.js
+++ client/src/router.js
@@ -1,13 +1,16 @@
-function Router({children}) {
-    <Route path="/questionnaire">
-    <Questionnaire />
-</Route>
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Questionnaire from './Questionnaire'; // Import your Questionnaire component
 
-    
-    return (
-      <div>
-        {children}
-      </div>
-    );
-  }
-  
+function Router({ children }) {
+  return (
+    <div>
+      {/* Define your routes here */}
+      <Route path="/questionnaire" component={Questionnaire} />
+      {/* Add more routes as needed */}
+      {children}
+    </div>
+  );
+}
+
+export default Router;
